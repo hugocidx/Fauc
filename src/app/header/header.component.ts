e77ed3c326341cafacc4882234809614c10ba6b9
hugo <hugocidx@gmail.com>
2024-01-01 22:34:40 -0300

    [FEAT] estructura de una vista

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component } from "@angular/core";
 import { AppComponent } from "../app.component";
+import { MaterialModule } from "src/assets/material.module";
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: "./header.component.html",
   styleUrls: ["header.component.scss"],
 })
 export class HeaderComponent {
+  isMenuOpen = false;
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
   constructor(private appComponent: AppComponent) {}
   scrollToSection(sectionId: string) {
     this.appComponent.scrollToSection(sectionId);
